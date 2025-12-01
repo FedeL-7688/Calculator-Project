a2f13bfb67f82d9a912b721611316d4b7ea210d2
FedeL <115208276+FedeL-7688@users.noreply.github.com>
2025-12-01 13:21:29 -0300

    almost finished logic!
    -managed to fix the issue of doing multiple operations before pressing equal. results mildly satisfactory.
    -added new "." button to operate with decimal numbers.
    -removed node modules which  were unused

--- script.js
+++ script.js
@@ -1,46 +1,30 @@
-
-
-// function add(x, y) {
-//   return x + y;
-// }
-// function subtract(x, y) {
-//   return x - y;
-// }
-// function multiply(x, y) {
-//   return x * y;
-// }
-// function divide(x, y) {
-//   return x / y;
-// }
-
-num1 = undefined
-option = undefined
-num2 = undefined
-result = 0
-
-function operate(a,operator,b) {
- 
-
+num1 = null;
+option = null;
+num2 = null;
+result = null;
+///operate function
+function operate(a, operator, b) {
+  const n1 = parseFloat(a);
+  const n2 = parseFloat(b);
   switch (operator) {
     case "+":
-   
-      return a + b;
-     
+      
+     return n1 + n2;
+      
+
     case "-":
-      return a - b;
+      return n1 - n2;
     case "*":
-      return a * b;
+      return n1 * n2;
     case "/":
-      if(b == 0){
+      if (n2== 0) {
         return "error: Can't divide by 0";
-        
       }
-      return a / b;
+      return n1 / n2;
     default:
       console.log("wrong operation");
   }
 }
-
 
 //calculator interface
 
@@ -48,7 +32,7 @@ calculatorContainer = document.querySelector(".container");
 calculator = document.createElement("div");
 calculator.id = "calculator";
 calculatorContainer.appendChild(calculator);
-histPlay = document.getElementById("history")
+histPlay = document.getElementById("history");
 inputCont = document.querySelector("#inputCont");
 buttonCont = document.querySelector("#buttonsCont");
 calculator.append(inputCont, buttonCont);
@@ -60,185 +44,209 @@ inputField.textContent = "hi";
 inputCont.append(inputField);
 //number buttons creation
 // number 0
-  numberZero = document.createElement("div")
-  const btn = document.createElement("button");
-  btn.classList.add("num");
-  btn.textContent = "0";
-  btn.addEventListener("click", () => {inputField.value += btn.textContent
- if (option == null){
-   num1 = inputField.value
-  }
-  else num2 = inputField.value.split(option)[1];
-  
-  });
-  numberZero.append(btn)
+numberZero = document.createElement("div");
+const btn = document.createElement("button");
+btn.classList.add("num");
+btn.textContent = "0";
+btn.addEventListener("click", () => {
+  inputField.value += btn.textContent;
+  if (option == null) {
+    num1 = inputField.value;
+  } else num2 = inputField.value.split(option)[1];
+});
+numberZero.append(btn);
 //from 1 to 3
-firstRow = document.createElement("div")
+firstRow = document.createElement("div");
 for (let i = 1; i <= 3; i++) {
   const btn = document.createElement("button");
   btn.classList.add("num");
   btn.textContent = i;
-  btn.addEventListener("click", () => {inputField.value += btn.textContent
- if (option == null){
-   num1 = inputField.value
-  }
-  else num2 = inputField.value.split(option)[1];
-  
-  }
-);
+  btn.addEventListener("click", () => {
+    inputField.value += btn.textContent;
+    if (option == null) {
+      num1 = inputField.value;
+    }
+    if (option != null) {
+      num2 = inputField.value.split(option)[1];
+    }
+  });
   firstRow.appendChild(btn);
- 
 }
 //from 4 to 6
-secondRow = document.createElement("div")
-for (let i = 4; i<=6;i++){
+
+secondRow = document.createElement("div");
+for (let i = 4; i <= 6; i++) {
   const btn = document.createElement("button");
   btn.classList.add("num");
   btn.textContent = i;
   btn.addEventListener("click", () => {
-    inputField.value += btn.textContent
-    if (option == null){
-   num1 = inputField.value
-  }
-  else num2 = inputField.value.split(option)[1];
-  
-  }
-);
+    inputField.value += btn.textContent;
+    if (option == null) {
+      num1 = inputField.value;
+    } else num2 = inputField.value.split(option)[1];
+  });
 
   secondRow.appendChild(btn);
-
 }
 //from 7 to 9
-thirdRow = document.createElement("div")
-for (let i = 7;i<=9;i++){
-    const btn = document.createElement("button");
+ thirdRow = document.createElement("div");
+ for (let i = 7; i <= 9; i++) {
+  const btn = document.createElement("button");
   btn.classList.add("num");
   btn.textContent = i;
   btn.addEventListener("click", () => {
-    inputField.value += btn.textContent
-  if (option == null){
-   num1 = inputField.value
-  }
-  else num2 = inputField.value.split(option)[1];
-
-  }
-);
+    inputField.value += btn.textContent;
+    if (option == null) {
+      num1 = inputField.value;
+    } else num2 = inputField.value.split(option)[1];
+  });
   thirdRow.appendChild(btn);
-}
-buttonCont.append(numberZero,firstRow,secondRow,thirdRow)
+ }
+ buttonCont.append(numberZero, firstRow, secondRow, thirdRow);
 
+
+
+//repeated operators function
+ function repOperation(optor) {
+  if (num1 !== null && num2 !== null ) {
+    result = operate(num1, option, num2);
+    console.log(result)
+    inputField.value = result + optor;
+    num1 = result;
+    num2 = null;
+    option = optor
+    return
+  } else if(inputField.value[inputField.value.length-1]==optor){
+    return
+  }
+  else {inputField.value += optor
+    option = optor
+  }
+ }
 
 ///operator buttons
-
 //add
- const addBtn = document.createElement("button")
- addBtn.classList.add("operators")
- addBtn.textContent = "+"
- calculator.append(addBtn)
- addBtn.addEventListener("click",()=> {inputField.value+= "+"
-  option = "+"})
+ const addBtn = document.createElement("button");
+  addBtn.classList.add("operators");
+  addBtn.textContent = "+";
+  calculator.append(addBtn);
+  addBtn.addEventListener("click", () => {
+   repOperation("+");
+   
+   });
 
- //subtract
+//subtract
 
- const subBtn = document.createElement("button")
- subBtn.classList.add("operators")
- subBtn.textContent = "-"
- calculator.append(subBtn)
+const subBtn = document.createElement("button");
+subBtn.classList.add("operators");
+subBtn.textContent = "-";
+calculator.append(subBtn);
 
- subBtn.addEventListener("click",()=> {inputField.value+= "-"
-  option = "-"})
+subBtn.addEventListener("click", () => {
+  repOperation("-")
+});
 
- //multiply
+//multiply
 
-const multiBtn = document.createElement("button")
- multiBtn.classList.add("operators")
- multiBtn.textContent = "*"
- calculator.append(multiBtn)
- multiBtn.addEventListener("click",()=> {inputField.value+= "*"
-  option = "*"})
+const multiBtn = document.createElement("button");
+multiBtn.classList.add("operators");
+multiBtn.textContent = "*";
+calculator.append(multiBtn);
+multiBtn.addEventListener("click", () => {
+  repOperation("*")
+});
 
- //divide
- 
- const divBtn = document.createElement("button")
- divBtn.classList.add("operators")
- divBtn.textContent = "/"
- calculator.append(divBtn)
- divBtn.addEventListener("click",()=> {inputField.value+= "/"
-  option = "/"})
+//divide
+
+const divBtn = document.createElement("button");
+divBtn.classList.add("operators");
+divBtn.textContent = "/";
+calculator.append(divBtn);
+divBtn.addEventListener("click", () => {
+  repOperation("/")
+});
 
 //equal
-const eqBtn = document.createElement("button")
- eqBtn.classList.add("operators")
- eqBtn.textContent = "="
- calculator.append(eqBtn)
-
- eqBtn.addEventListener("click",()=>{
-
-
-const n1 = parseFloat(num1)
-const n2 = parseFloat(num2)
-
-result = operate(n1,option,n2)
+ const eqBtn = document.createElement("button");
+ eqBtn.classList.add("operators");
+ eqBtn.textContent = "=";
+ calculator.append(eqBtn);
  
-inputField.value = Math.round((result + Number.EPSILON) * 100) / 100
-num1 = inputField.value
- }
-)
- 
+ eqBtn.addEventListener("click", () => {
+  
+  if (num1 !== null && option !== null && num2 !== null) {
+    result = operate(num1, option, num2);
+    inputField.value = Math.round((result + Number.EPSILON) * 100) / 100;
+    num1 = inputField.value;
+    num2 = null;
+    option = null;
+  }
+  console.log(inputField.value)
+  });
+
+//decimal Point
+
+decPoint = document.createElement("button")
+decPoint.classList.add("operators")
+decPoint.textContent = ".";
+calculator.append(decPoint)
+
+decPoint.addEventListener("click",()=>{
+  let currNum = option == null? inputField.value : inputField.value.split(option)[1]
+  if(currNum && currNum.includes(".")){
+    return
+  }else inputField.value += ".";
+})
 
 //clear
-const clearBtn = document.createElement("button")
-clearBtn.classList.add("operators")
-clearBtn.textContent = "C"
-calculator.append(clearBtn)
-clearBtn.addEventListener("click",(e)=>{
-  e.preventDefault()
-  inputField.value = ""
-  num1 = undefined
-  option = undefined
-  num2 = undefined
-})
+const clearBtn = document.createElement("button");
+clearBtn.classList.add("operators");
+clearBtn.textContent = "C";
+calculator.append(clearBtn);
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputField.value = "";
+  num1 = null;
+  option = null;
+  num2 = null;
+});
 // delete Button
-const deleteBtn = document.createElement("button")
-deleteBtn.classList.add("operators")
-deleteBtn.textContent = "D"
-calculator.append(deleteBtn)
-deleteBtn.addEventListener("click",()=>{
-  
-  inputField.value = inputField.value.slice(0,-1)
-
+const deleteBtn = document.createElement("button");
+deleteBtn.classList.add("operators");
+deleteBtn.textContent = "D";
+calculator.append(deleteBtn);
+deleteBtn.addEventListener("click", () => {
+  inputField.value = inputField.value.slice(0, -1);
 
   const total = inputField.value;
 
   if (!option) {
-    num1 = total || undefined;
-    num2 = undefined;
+    num1 = total || null;
+    num2 = null;
   } else {
-    const splitted = text.split(option);
-
-    num1 = splitted[0] || undefined;
-    num2 = splitted[1] || undefined;
-
+    const splitted = total.split(option);
+    num1 = splitted[0] || null;
+    num2 = splitted[1] || null;
     if (total[total.length - 1] === option) {
-      num2 = undefined;
+      num2 = null;
     }
     if (!total.includes(option)) {
-      option = undefined;
-      num2 = undefined;
+      option = null;
+      num2 = null;
     }
   }
-})
-
-///
-operators = document.querySelectorAll(".operators")
-opCont = document.createElement("div")
-opCont.id = "opCont"
-operators.forEach(element => {
-  opCont.appendChild(element) 
 });
-calculator.append(opCont)
+
+///all operators selection
+operators = document.querySelectorAll(".operators");
+opCont = document.createElement("div");
+opCont.id = "opCont";
+operators.forEach((element) => {
+  opCont.appendChild(element);
+});
+calculator.append(opCont);
 
 ///general buttons container
-btnsCont = document.createElement("div")
-btnsCont.append(buttonCont,opCont)
-calculator.append(btnsCont)
+btnsCont = document.createElement("div");
+btnsCont.append(buttonCont, opCont);
+calculator.append(btnsCont);
